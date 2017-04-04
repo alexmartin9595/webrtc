8cb69d6012ac14c94beee9fada3b95338e3e270d
AlexMartsinkevich <Alex.Martsinkevich@softensity.com>
2017-04-04 19:39:23 +0300

    added adapter js to project; implemened media capture; implemented server side

--- WebRTC/Hub/ConferenceHub.cs
+++ WebRTC/Hub/ConferenceHub.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
@@ -10,9 +11,49 @@ namespace WebRTC.Hub
     [HubName("conference")]
     public class ConferenceHub : Microsoft.AspNet.SignalR.Hub
     {
-        public void Hello()
+        private static readonly Dictionary<string, string> _connections = new Dictionary<string, string>();
+
+        public static Dictionary<string, string> Connections
         {
-            Clients.All.hello();
+            get { return _connections; }
+        }
+
+        public async Task OnConnected(string userId)
+        {
+            _connections.Add(Context.ConnectionId, userId);
+            await base.OnConnected();
+            Clients.All.onUserConnected(userId);
+        }
+
+        public async Task OnDisconnected(string userId, bool stopCalled)
+        {
+            _connections.Remove(Context.ConnectionId);
+            await base.OnDisconnected(stopCalled);
+        }
+
+
+        public void SetOffer(object description, string offererId)
+        {
+            foreach (var connection in _connections)
+            {
+                Clients.Client(connection.Key).onOfferReceived(description);
+            }
+        }
+
+        public void AddCandidate(string contentId, object candidate, string recipientId)
+        {
+            foreach (var connection in _connections)
+            {
+                Clients.Client(connection.Key).onCandidateReceived(candidate);
+            }
+        }
+
+        public void SetAnswer(string contentId, object description, string offererId)
+        {
+            foreach (var connection in _connections)
+            {
+                Clients.Client(connection.Key).onAnswerReceived(offererId);
+            }
         }
 
 
--- WebRTC/Scripts/conference.js
+++ WebRTC/Scripts/conference.js
@@ -1,10 +1,143 @@
 ﻿"use strict";
 (function () {
+    var attachedVideos = [];
+    var peerConnections = {};
+    var participants = [];
+    var localStream = null;
+    var localUserId = "";
+    var MediaType = { AUDIO: "audio", VIDEO: "video" };
+    var VideoType = { CAMERA: "camera", DESKTOP: "desktop" };
+    var options = {
+        servers: {
+            iceServers: [
+                { urls: ["stun:stun.l.google.com:19302"] }
+            ]
+        },
+        offerOptions: {
+            offerToReceiveAudio: 1,
+            offerToReceiveVideo: 1
+        }
+    }
 
     var connection = $.hubConnection();
     connection.logging = true;
     var hub = connection.createHubProxy('conference');
-    connection.start().done(function () {
-        hub.invoke.apply(hub, ["Hello"]);
+    hub.on('onUserConnected', function (userId) {
+        if (userId != localUserId) {
+            createOffer(userId);
+        }
     });
+    connection.start().done(function () {
+        localUserId = generateId();
+        invokeHubCall("OnConnected", localUserId);
+        createLocalStream(onLocalStreamCreated, onError, false, localUserId);
+
+       
+    });
+
+    function invokeHubCall() {
+        hub.invoke.apply(hub, arguments);
+    }
+
+
+    function createLocalStream(success, error, isScreenSharing, userId) {
+        return navigator.getUserMedia({
+            audio: true,
+            video: isScreenSharing ? { mediaSource: "window" || "screen" } : { width: 1280, height: 720 }
+        }, success.bind(null, userId), error);
+    }
+
+    function onLocalStreamCreated(userId, stream) {
+        var $streamElement = "<video autoplay='1' data-id='" + userId + "' />";
+        $(".videoRecordingWrapper").append($streamElement);
+        localStream = stream;
+        $('video[data-id="' + userId + '"]')[0].srcObject = stream;
+    }
+
+    function onError() {
+        
+    }
+
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+
+    function generateId() {
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
+    }
+
+
+
+
+
+    function createOffer(userId) {
+
+        var pc = new RTCPeerConnection(options.servers);
+
+        bindPeerConnectionEvents(pc, userId);
+
+        pc.addStream(localStream);
+
+        pc.createOffer(options.offerOptions).then(onCreateOfferSuccess.bind(null, userId));
+
+        peerConnections[userId] = pc;
+    }
+
+    function onOfferCreated(description, recipientId) {
+        invokeHubCall("SetOffer", description, recipientId);
+    }
+
+    function onCreateOfferSuccess(userId, desc) {
+        console.log('Offer created');
+
+        peerConnections[userId].setLocalDescription(desc).then(function () {
+            console.log('local description is set after offer creation');
+        });
+        onOfferCreated(new RTCSessionDescription(desc), userId);
+        console.log('offer sent');
+    }
+
+    function onIceCandidate(userId, event) {
+        var ice = event.candidate;
+        if (!ice)
+            return;
+
+        console.log(ice.candidate);
+        onCandidateCreated(ice, userId);
+    }
+
+    function bindPeerConnectionEvents(pc, userId) {
+        pc.onicecandidate = onIceCandidate.bind(null, userId);
+        pc.onaddstream = receiveRemoteStream();
+    }
+
+    function onCandidateCreated() {
+        
+    }
+
+    function attachVideoStream(videoStream, participant) {
+        var $streamVideoElement = options.getVideoElementForUser(participant);
+        
+        $streamVideoElement[0].srcObject = stream;
+    }
+
+    function receiveRemoteStream(e) {
+        var participant;
+
+        if (participants.length == 1) {
+            participant = participants[0];
+        }
+        else {
+            participant = participants.filter(function (participant) {
+                return attachedVideos.indexOf(participant) == -1;
+            })[0];
+        }
+
+        attachedVideos.push(participant);
+
+        attachVideoStream(e.stream, participant);
+    }
 })();
